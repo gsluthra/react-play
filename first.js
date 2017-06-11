0c5166db681037400c180a49450e1e20f32c2748
Gurpreet Luthra <gluthra@thoughtworks.com>
2017-06-11 09:29:07 +0530

    Rewriting the createClass with arrow functions. Can do this too when all we need is the render() method, and no state.

--- first.js
+++ first.js
@@ -1,9 +1,18 @@
 
-  var MyHelloTag = React.createClass({
-  		render: function() {
-  			return(<div> <h1>Hello Tag</h1> </div>);
-  		}
-  });
+  // var MyHelloTag = React.createClass({
+  // 		render: function() {
+  // 			return(<div> <h1>Hello Tag</h1> </div>);
+  // 		}
+  // });
+
+  var MyChildTag = () => {
+  	return(<div> Child Tag</div>);
+  };
+
+
+  var MyHelloTag = () => {
+  	return(<div> <h1>Hello Tag1</h1> <MyChildTag/> </div>);
+  };
 
   ReactDOM.render(
     <MyHelloTag/>,
