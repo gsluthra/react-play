db341eba5ac14a111b92daac0f0504dc3fc6e6a3
Gurpreet Luthra <gluthra@thoughtworks.com>
2017-06-18 19:57:14 +0530

    Handle an event using react class, or using a function

--- first.js
+++ first.js
@@ -1,4 +1,36 @@
 
+
+//Written using a function
+var MyActionHandler = () => {
+
+    //Function to handle the click event
+	const handleClick = (e) => {
+    	e.preventDefault();
+    	alert('The link was clicked.');
+    };
+
+	return (
+		<a href="#" onClick={handleClick}>Click Me</a>
+	);
+};
+
+//Written using a class. Note the this.handleClick call
+class MyActionHandlerClass extends React.Component {
+
+    
+	handleClick(e) {
+    	e.preventDefault();
+    	console.log('The link CLICK ME HERE was clicked.');
+    }
+
+    render() {
+		return (
+			<a href="#" onClick={this.handleClick}>Click Me Here</a>
+		);    	
+    }
+}
+
+
 class MyClock extends React.Component {
 	constructor(props) {
 		super(props);
@@ -25,7 +57,7 @@ class MyClock extends React.Component {
 	    	  date: new Date()
 	    	}
     	);
-  	}
+  	}	
 
 }
 
@@ -34,6 +66,9 @@ var MyHelloTag = () => {
 		<div> 
 			<h1>Hello Clock</h1> 
 			<MyClock/> 
+			<MyActionHandler/>
+			<br/>
+			<MyActionHandlerClass/>
 		</div>
 		);
 };
