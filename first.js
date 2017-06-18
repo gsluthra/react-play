4cab35348f917522e85c9edf30fc27b98e942e91
Gurpreet Luthra <gluthra@thoughtworks.com>
2017-06-18 20:42:19 +0530

    Added a toggle button which changes its state using prevState

--- first.js
+++ first.js
@@ -1,12 +1,39 @@
 
+class MyToggle extends React.Component {
 
-//Written using a function
+	constructor(props) {
+		super(props);
+		this.handleToggleClick = this.handleToggleClick.bind(this);
+		this.state = {
+			isToggleOn: false
+		};
+	}
+
+	handleToggleClick(e) {
+
+		this.setState(
+			(prevState) => ({isToggleOn: !prevState.isToggleOn})
+		);
+	}
+
+
+	render() {
+		return (
+			<button onClick={this.handleToggleClick} >
+				{this.state.isToggleOn ? 'State: ON' : 'State: OFF'}
+			</button>
+			);
+	}
+}
+
+
+//MyActionHandler Written using a function
 var MyActionHandler = () => {
 
     //Function to handle the click event
 	const handleClick = (e) => {
     	e.preventDefault();
-    	alert('The link was clicked.');
+    	console.log('The link was clicked. THIS: '+ this);
     };
 
 	return (
@@ -16,16 +43,19 @@ var MyActionHandler = () => {
 
 //Written using a class. Note the this.handleClick call
 class MyActionHandlerClass extends React.Component {
-
+	constructor(props) {
+		super(props);
+		this.handleClick1 = this.handleClick1.bind(this); //To ensure this get bound with handleclick1
+	}
     
-	handleClick(e) {
+	handleClick1(e) {
     	e.preventDefault();
-    	console.log('The link CLICK ME HERE was clicked.');
+    	console.log('The link CLICK ME HERE was clicked. THIS: '+ this);
     }
 
     render() {
 		return (
-			<a href="#" onClick={this.handleClick}>Click Me Here</a>
+			<a href="#" onClick={this.handleClick1}>Click Me Here</a>
 		);    	
     }
 }
@@ -69,6 +99,9 @@ var MyHelloTag = () => {
 			<MyActionHandler/>
 			<br/>
 			<MyActionHandlerClass/>
+			<br/>
+			<br/>
+			<MyToggle/>
 		</div>
 		);
 };
