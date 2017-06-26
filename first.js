9c40d278523ab7a19f58fe0a271ab013de593775
Gurpreet Luthra <gluthra@thoughtworks.com>
2017-06-26 13:10:08 +0530

    Refactored to have a single data value in state -- celcius, and then its rendered appropriately in both children.

--- first.js
+++ first.js
@@ -242,8 +242,7 @@ class MyTemperatureEnteringWidget extends React.Component {
 		super(props);
 
 		this.state = {
-			c_temp : 0,
-			f_temp : this.convertCtoF(0)
+			c_temp : 0
 		};
 
 		this.handleOnChangeValue = this.handleOnChangeValue.bind(this);
@@ -261,36 +260,43 @@ class MyTemperatureEnteringWidget extends React.Component {
 	handleOnChangeValue(e) {
 		const controlName = e.target.name;
 		const temperature = e.target.value;
-		let f_temp = -1000, c_temp = -1000; //some arbitary outside value
+		
+		if(temperature === ''){
+			this.setState({
+				c_temp : ''
+			});				
+			return;
+		}
+		
+		let celcius_temp = 0; //some arbitary value
 
 		if(controlName === "celcius"){
 			console.log('temperature entered in celcius control. recvd value = '+ temperature);
-			c_temp = temperature;
-			f_temp = this.convertCtoF(temperature);
-
+			celcius_temp = temperature;
 		} else if (controlName === 'farenheit') {
 			console.log('temperature entered in farenheit control. recvd value = '+ temperature);
-			f_temp = temperature;
-			c_temp = this.convertFtoC(temperature);
+			celcius_temp = this.convertFtoC(temperature);
 		} else {
 			console.log('WTF. Which control did this come from? Control Name: '+ controlName);
 		}
 
 		this.setState({
-			f_temp : [f_temp],
-			c_temp : [c_temp]
-		});
+			c_temp : [celcius_temp]
+		});				
 
 	}
 
 	render() {
 
+        const celcius_temp = this.state.c_temp;
+        const farenheit_temp = (celcius_temp === '')? '': this.convertCtoF(celcius_temp);
+
 		return (
 			<div>
 				<fieldset>
 					<legend> TEMP CONTROL </legend>
-					<MyTemperatureEnteringComponent type='celcius' temperatureValue={this.state.c_temp} onChangeFunction={this.handleOnChangeValue}/>
-					<MyTemperatureEnteringComponent type='farenheit' temperatureValue={this.state.f_temp} onChangeFunction={this.handleOnChangeValue}/>
+					<MyTemperatureEnteringComponent type='celcius' temperatureValue={celcius_temp} onChangeFunction={this.handleOnChangeValue}/>
+					<MyTemperatureEnteringComponent type='farenheit' temperatureValue={farenheit_temp} onChangeFunction={this.handleOnChangeValue}/>
 
 				</fieldset>
 			</div>
